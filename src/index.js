1b14c9f15d778f5d7b24b2d165a49f8d06303d72
AlenaBDE <bde.alena@gmail.com>
2018-02-27 14:54:04 +0300

    '6'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+        var count = 0;
+        for (var i = 5; i < number + 1; i = i + 5) {
+            if ((i % 5) == 0) {
+                count++;
+                if ((i / 5) % 5 == 0) {
+                    var N = (i / 5);
+                    var D = 5;
+                    while (N > 1) {
+                        if (N % D == 0) {
+                            count++;
+                            N = (N / D >> 0);
+                        }
+                        else {
+                            N = 0;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    };
+
